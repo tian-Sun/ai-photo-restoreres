c35e5650d94d71e522303e49ec76f7607b0ed67d
Ava <tianhuiting@hzanchu.com>
2025-03-28 22:40:52 +0800

    fix: 添加 API 路由的 CORS 配置

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ module.exports = {
     domains: ["upcdn.io", "replicate.delivery", "lh3.googleusercontent.com"],
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
