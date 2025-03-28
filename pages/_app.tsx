291d297eb78a5a73bfea0f67e58d426f534a5f6f
Ava <tianhuiting@hzanchu.com>
2025-03-28 21:00:43 +0800

    fix: 修复 session 类型错误

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
-import type { AppProps } from 'next/app';
-// import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import PlausibleProvider from 'next-plausible';
+import { Session } from 'next-auth';
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{
+  session: Session;
+}>) {
   return (
-    // <SessionProvider session={session}>
+    <SessionProvider session={session}>
       <PlausibleProvider domain='restorephotos.io'>
         <Component {...pageProps} />
       </PlausibleProvider>
-    // </SessionProvider>
+    </SessionProvider>
   );
 }
 
